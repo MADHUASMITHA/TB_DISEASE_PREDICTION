f0f02e60bbcf776de7ffd61f4724d94392d32350
MADHUASMITHA <madhuasmitha2525@gmail.com>
2025-12-10 17:08:17 +0000

    Initial commit

---
+++ upload.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Replace YOUR_TOKEN_HERE with your actual GitHub token
+echo Then run these commands:
+echo.
+echo git remote set-url origin https://YOUR_TOKEN_HERE@github.com/MADHUASMITHA/TB_DISEASE_PREDICTION.git
+echo git push -u origin main
+pause
